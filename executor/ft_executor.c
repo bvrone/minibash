88ddeba1d6331b1513aef147b17d02b9d3b9de87
Atrics Glady <aglady@et-a2.kzn.21-school.ru>
2021-04-29 20:58:49 +0300

    исправлена обрабтка pipe (убраны fork)

--- executor/ft_executor.c
+++ executor/ft_executor.c
@@ -47,11 +47,8 @@ int	exec_pipeline(t_cmds_pipeline *pipeline, int *tmp)
 {
 	size_t	n;
 	int		i;
-	int		k;
 	int		res;
-	int		status;
 	int		**pipe_fd;
-	int		*pid;
 
 
 	n = ft_lstsize(pipeline->cmds);
@@ -67,103 +64,43 @@ int	exec_pipeline(t_cmds_pipeline *pipeline, int *tmp)
 		if (pipe(pipe_fd[i]) < 0)
 			return (1);
 	}
-	pid = malloc(n * sizeof(int));
-	if (!pid)
-		error_exit("malloc", "Memory allocation failed", 2);
-	pid[0] = fork();
-	if (pid[0] < 0)
-		return (1);
-	if (pid[0] == 0)
-	{
-		close(pipe_fd[0][0]);
-		k = 0;
-		while (++k < n - 1)
-		{
-			close(pipe_fd[k][0]);
-			close(pipe_fd[k][1]);
-		}
-		if (pipeline->fdin != -1)
-			dup2(pipeline->fdin, 0);
-		else
-			dup2(tmp[0], 0);
-		dup2(pipe_fd[0][1], 1);
-		res = execute_builtins(pipeline, pipeline->cmds);
-		if (res == -1)
-			execute_not_builtins(pipeline, pipeline->cmds);
-		if (pipeline->fdin != -1)
-			close(pipeline->fdin);
-		close(pipe_fd[0][1]);
-		return (res);
-	}
+	if (pipeline->fdin != -1)
+		dup2(pipeline->fdin, 0);
+	else
+		dup2(tmp[0], 0);
+	dup2(pipe_fd[0][1], 1);
+	res = execute_builtins(pipeline, pipeline->cmds);
+	if (res == -1)
+		execute_not_builtins(pipeline, pipeline->cmds);
+	if (pipeline->fdin != -1)
+		close(pipeline->fdin);
+	close(pipe_fd[0][1]);
 	i = 0;
 	while (++i < n - 1)
 	{
-		pid[i] = fork();
-		if (pid[i] < 0)
-			return (1);
-		if (pid[i] == 0)
-		{
-			k = -1;
-			while (++k < n - 1)
-			{
-				if (k == i - 1)
-					continue ;
-				close(pipe_fd[k][0]);
-			}
-			k = -1;
-			while (++k < n - 1)
-			{
-				if (k == i)
-					continue ;
-				close(pipe_fd[k][1]);
-			}
-			dup2(pipe_fd[i - 1][0], 0);
-			dup2(pipe_fd[i][1], 1);
-			res = execute_builtins(pipeline, ft_lstind(pipeline->cmds, i));
-			if (res == -1)
-				execute_not_builtins(pipeline, ft_lstind(pipeline->cmds, i));
-			close(pipe_fd[i - 1][0]);
-			close(pipe_fd[i][1]);
-			return (res);
-		}
-	}
-	pid[n - 1] = fork();
-	if (pid[n - 1] < 0)
-		return (1);
-	if (pid[n - 1] == 0)
-	{
-		close(pipe_fd[n - 2][1]);
-		k = -1;
-		while (++k < n - 2)
-		{
-			close(pipe_fd[k][0]);
-			close(pipe_fd[k][1]);
-		}
-		dup2(pipe_fd[n - 2][0], 0);
-		if (pipeline->fdout != -1)
-			dup2(pipeline->fdout, 1);
-		else
-			dup2(tmp[1], 1);
-		res = execute_builtins(pipeline, ft_lstind(pipeline->cmds, n - 1));
+		dup2(pipe_fd[i - 1][0], 0);
+		dup2(pipe_fd[i][1], 1);
+		res = execute_builtins(pipeline, ft_lstind(pipeline->cmds, i));
 		if (res == -1)
-			execute_not_builtins(pipeline, ft_lstind(pipeline->cmds, n - 1));
-		close(pipe_fd[n - 2][0]);
-		if (pipeline->fdout != -1)
-			close(pipeline->fdout);
-		return (res);
+			execute_not_builtins(pipeline, ft_lstind(pipeline->cmds, i));
+		close(pipe_fd[i - 1][0]);
+		close(pipe_fd[i][1]);
 	}
-	i = -1;
-	while (++i < n)
-	{
-		waitpid(pid[i], &status, 0);
-		pipeline->last_ret_code = WEXITSTATUS(status);
-	}
-	// free pipe_fd and pid
+	dup2(pipe_fd[n - 2][0], 0);
+	if (pipeline->fdout != -1)
+		dup2(pipeline->fdout, 1);
+	else
+		dup2(tmp[1], 1);
+	res = execute_builtins(pipeline, ft_lstind(pipeline->cmds, n - 1));
+	if (res == -1)
+		execute_not_builtins(pipeline, ft_lstind(pipeline->cmds, n - 1));
+	close(pipe_fd[n - 2][0]);
+	if (pipeline->fdout != -1)
+		close(pipeline->fdout);
 	i = -1;
 	while (++i < n)
 		free(pipe_fd[i]);
 	free(pipe_fd);
-	free(pid);
 	return (pipeline->last_ret_code);
 }
 
